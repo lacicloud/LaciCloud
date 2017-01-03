93e5d7972bc2a216d600a5eed94597190ef17f05
Molnarfi Laszlo <laci@lacicloud.net>
2017-01-03 16:53:56 +0100

    Added dependencies, secrets.ini, and fixed a bug in the clear_unconfirmed_mysql.sh script

--- config/readme.txt
+++
@@ -1,4 +0,0 @@
-All private username's and password's have been replaced by username:password!
-
-Currently development settings are in place (PHP show error's is ON!) 
-In nginx webcam.conf is for my RPI security camera that watches my door.
--- scripts/clear_unconfirmed_mysql.sh
+++ scripts/clear_unconfirmed_mysql.sh
@@ -4,8 +4,8 @@
 date=$(date +"%T")
 
 #cfget is required
-username = `cfget -C /var/ftp/www/developers/secrets.ini secrets/deletebot_user`
-password = `cfget -C /var/ftp/www/developers/secrets.ini secrets/deletebot_password`
+username=`cfget -C /var/ftp/www/developers/secrets.ini secrets/deletebot_user`
+password=`cfget -C /var/ftp/www/developers/secrets.ini secrets/deletebot_password`
 
 /usr/bin/mysql -u "$username" --password="$password" -e 'USE laci_corporations_users; DELETE FROM users WHERE unique_id != 1;'
 
