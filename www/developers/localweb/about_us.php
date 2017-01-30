debd764b8ecacdf477fdb589a81687354bb5424e
Laci <laci@aperture.laboratories>
2017-01-30 22:16:05 +0100

    Added the about_us page and style.css

---
+++ www/developers/localweb/about_us.php
@@ -0,0 +1,155 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <title>LaciCloud</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link rel="icon" type="image/png" href="/resources/favicon-32x32.png">
+    <!--styles-->
+    <link href="/css/style.css" rel="stylesheet" />
+</head>
+<body class="body-about_us">
+    <!--start - main menu-->
+    <nav>
+        <ul class="topnav" id="mainNav">
+            <li><a class="menu-link" href="/">Home</a><span class="menu-dot">.</span></li>
+            <li><a class="menu-link active" href="/about_us">About Us</a><span class="menu-dot">.</span></li>
+            <li><a class="menu-link" href="/docs">Help & Others</a></li>
+            <li class="logo-li">
+                <img class="logo-img" src="/resources/laci-logo.png" />
+            </li>
+            <li><a class="menu-link" href="/shop">Shop</a><span class="menu-dot">.</span></li>
+            <li><a class="menu-link" href="/contact">Contact</a><span class="menu-dot">.</span></li>
+            <li><a class="menu-link" href="/login">Log in/Sign up</a></li>
+            <li class="icon">
+                <a href="javascript:void(0);" onclick="navFunction()">&#9776;</a>
+            </li>
+        </ul>
+    </nav>
+    <!--end - main menu-->
+    <!--start - header-->
+    <section class="h-header">
+        <div class="col-3 topheader">
+        </div>
+    </section>
+
+    <!--start - about-->
+    <section class="row h-about about" id="about">
+        <div class="col-3 text-center">
+            <div class="section-icon"><img src="/resources/icon-whylaci.png" /></div>
+            <div class="section-heading"><h1>Why LaciCloud?</h1></div>
+            <div class="section-text about-text">
+               Why did we create LaciCloud? Simply put, we have tried to find a cloud storage service for personal use. What was available didn't fit us. They aren't flexible, they aren't open-source, they don't allow you to encrypt your stuff, and they most certainly breach your constitutional rights by keeping an eye on your stuff so to say. Back in 2013, my Dropbox account was suspended due to allegedly uploading pirated software to the site; that's when I thought I had enough. By creating LaciCloud, I created a secure environment for myself and my friends where they could upload files in complete security and privacy to an FTP server. This quickly grew into LaciCloud. <br><strong>LaciCloud is the hard work of multiple people:</strong>
+                <!-- Quick, simple, flexible => IT REALLY ISN'T ROCKET SCIENCE! -->
+            </div>
+        </div>
+    </section>
+
+    <div class="clear"></div>
+
+    <!--start - features-->
+    <section class="row h-features features">
+        <div class="feature-block">
+            <div class="text-center content-h-400">
+                <div class="section-icon"><img src="/resources/versatility-icon.png" /></div>
+                <div class="section-heading"><h1>Versatility</h1></div>
+                <div class="section-text pad-lr versatility-text">
+  <strong>Head Of ICT</strong><br>Laci is our excellent prograrnmer. He is an aspiring young computer scientist with the desire to create software that makes people happy. He has experience in web development, web app and network security, computer forensics and system administration. He has been passionate about computers since the age of 4, and probably will be forever. 
+                    <span class="section-end">.</span>
+                </div>
+
+            </div>
+        </div>
+        <div class="feature-block">
+            <div class="text-center content-h-400">
+                <div class="section-icon"><img src="/resources/icon-features.png" /></div>
+                <div class="section-heading"><h1>Features</h1></div>
+                <div class="section-icon"><img src="/resources/compatibility-icon.png" /></div>
+                <div class="section-heading"><h1>Privacy</h1></div>
+                <div class="section-text pad-lr versatility-text">
+                        <ul>
+                                <li>Test</li>
+                        </ul>
+                    <span class="section-end">.</span>
+                </div>
+
+            </div>
+        </div>
+        <div class="feature-block">
+            <div class="text-center content-h-400">
+                <div class="section-icon"><img src="/resources/versatility-icon.png" /></div>
+                <div class="section-heading"><h1>Security</h1></div>
+                <div class="section-text pad-lr versatility-text">
+                                <strong>Head Of ICT</strong><br>Laci is our excellent prograrnmer. He is an aspiring young computer scientist with the desire to create software that makes people happy. He has experience in web development, web app and network security, computer forensics and system administration. He has been passionate about computers since the age of 4, and probably will be forever. 
+                    <span class="section-end">.</span>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <div class="clear"></div>
+    <div class="clearfix"></div>
+
+    <!--start - meet the team-->
+    <section class="row h-features meettheteam">
+        <div class="meet-the-team">
+            <span>Meet the team!</span>
+        </div>
+        <div class="team-wrapper">
+            <div class="team-row">
+                <div class="team-user">
+                    <div class="team-name">Molnarfi Laszlo</div>
+                    <span class="team-end">.</span>
+                    <div class="team-description">
+                        <strong>Head Of ICT</strong><br>Laci is our excellent prograrnmer. He is an aspiring young computer scientist with the desire to create software that makes people happy. He has experience in web development, web app and network security, computer forensics and system administration. He has been passionate about computers since the age of 4, and probably will be forever. 
+                    </div>
+                </div>
+                <div class="team-image">image</div>
+            </div>
+            <div class="team-row">
+                <div class="team-image">image</div>
+                <div class="team-user">
+                    <div class="team-name">Tristan Thomson</div>
+                    <span class="team-end">.</span>
+                    <div class="team-description">
+                        <strong>Head of Design</strong><br>Tristan is our excellent designer. He has been in the design industry for ,at as of now. He is highly skilled in graphic design, and an excellent, dedicated worker. He has completed over a 100 graphic design projects to date all with great success...
+                    </div>
+                </div>
+            </div>
+            <div class="team-row">
+                <div class="team-user">
+                    <div class="team-name">Fabio Barbero</div>
+                    <span class="team-end">.</span>
+                    <div class="team-description">
+                        <strong>Head of User Experience and Testing</strong><br> Fabio is our excellent Beta tester. Over the past few months, Fabio has been greatly dedicated to this project. Finding every little bug, content problem, spelling mistake and always had great suggestions for the site. He is also a young aspiring computer scientist like Laci. In addition to these team members, i feel it is neccesarry to mention some amazing people who have helped us on our journey to create LaciCloud; Click <strong><a style="color:inherit;text-decoration:none;" href="/humans.txt">here</a></strong> to read more about these other members.
+                    </div>
+                </div>
+                <div class="team-image">image</div>
+            </div>
+        </div>
+    </section>
+
+    <div class="clear"></div>
+
+    <!--start - footer-->
+    <footer class="footer-wrapper">
+        <div class="footer-leftbox">
+            <span class="text-uppercase bold-font">LaciCloud</span><br />
+            <span class="text-italic">Made with love</span><br />
+            <span class="text-italic">Brussels, Belgium  - <a href="mailto:laci@lacicloud.net">laci@lacicloud.net</a></span>
+        </div>
+        <div class="footer-social">
+            <a href="#" class="icon tw"><img src="/resources/social-twitter.png" /></a>
+            <a href="#" class="icon fb"><img src="/resources/social-facebook.png" /></a>
+            <a href="#" class="icon yt"><img src="/resources/social-youtube.png" /></a>
+        </div>
+    </footer>
+    <!--scripts-->
+    <script src="/js/main.js"></script>
+</body>
+
+</html>
