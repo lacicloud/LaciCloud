2548eec28f4002f14afd7e7761c866dcc6f3ef63
Molnarfi Laszlo <laci@lacicloud.net>
2017-02-07 23:51:16 +0100

    Added resources, error.php, js folder,  humans.txt, deleted unecccesary Mobile Detect library, added GoURL callback page

---
+++ www/developers/localweb/error.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>404 Not Found</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+
+</head>
+
+<body>
+
+
+<img src="/resources/404.jpg" style="width:100%;height:100%;position:absolute;top:0;left:0;" alt="404 Not Found">
+
+<script>
+
+document.onkeypress = function (e) {
+    e = e || window.event;
+    	window.history.go(-1);
+};
+
+</script>
+
+</body>
+</html>
