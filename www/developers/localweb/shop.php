de54239979b5793ce0a411bc1b3f1acc16ff930b
Aleksandar Barticevic <abarticevic@gmail.com>
2017-02-12 21:21:53 +0100

    Shop page
    Shop page

---
+++ www/developers/localweb/shop.php
@@ -0,0 +1,146 @@
+﻿<html>
+<head>
+    <title>LaciCloud</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/png" href="/resources/favicon-32x32.png">
+    <!--styles-->
+    <link href="/css/style.css" rel="stylesheet">
+</head>
+<body class="body-shop">
+    <!--start - main menu-->
+    <nav>
+        <ul class="topnav" id="mainNav">
+            <li><a class="menu-link" href="/">Home</a><span class="menu-dot">.</span></li>
+            <li><a class="menu-link active" href="/about_us">About Us</a><span class="menu-dot">.</span></li>
+            <li><a class="menu-link" href="/docs">Help &amp; Others</a></li>
+            <li class="logo-li">
+                <img class="logo-img" src="/resources/laci-logo.png">
+            </li>
+            <li><a class="menu-link" href="/shop">Shop</a><span class="menu-dot">.</span></li>
+            <li><a class="menu-link" href="/contact">Contact</a><span class="menu-dot">.</span></li>
+            <li><a class="menu-link" href="/login">Log in/Sign up</a></li>
+            <li class="icon">
+                <a href="javascript:void(0);" onclick="navFunction()">☰</a>
+            </li>
+        </ul>
+    </nav>
+    <!--end - main menu-->
+    <!--start - header-->
+    <section class="h-header">
+        <div class="col-3 topheader">
+        </div>
+    </section>
+
+    <!--start - about-->
+    <section class="row h-shop shop" id="shop">
+        <div class="col-3 text-center">
+            <div class="section-icon"><img src="/resources/icon-whylaci.png"></div>
+            <div class="section-heading"><h1>We believe in choice.</h1></div>
+            <div class="section-text about-text">
+                We think it's always better to have something that suits you anmd just you.
+                That's why we bring you a range of choice. Whoever you are, whatever your needs are, we have something for tou!
+                If you don't feel that the plans we offer suit you, contact us and we'll try to tailor something to you.
+                blah blah blah blah blah blah blah blah blah
+            </div>
+        </div>
+    </section>
+
+    <div class="clear"></div>
+
+    <!--start - meet the team-->
+    <section class="row h-features meettheteam">
+        <div class="team-wrapper">
+            <div class="team-row team-row-1">
+                <div class="team-image"><img style="height:100%; width:100%" src="/resources/laci.jpg"></div>
+                <div class="team-user user-1">
+                    <div class="team-name">Free - For First Timers</div>
+                    <span class="team-end">.</span>
+                    <div class="team-description">
+                        <ul>
+                            <li>250 mb storage</li>
+                            <li>One user</li>
+                            <li>Free technical support</li>
+                            <li>Limited bandwidth</li>
+                            <li>Limited to 3 devices</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="team-row team-row-2">
+                <div class="team-user user-2">
+                    <div class="team-name">10€/Month - For regulars</div>
+                    <span class="team-end">.</span>
+                    <div class="team-description">
+                        <ul>
+                            <li>15 gb storage</li>
+                            <li>Unlimited users</li>
+                            <li>Free technical support</li>
+                            <li>50 mb/s bandwidth</li>
+                            <li>Unlimited devices</li>
+                            <li>Host static websites</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="team-image"><img style="height:100%; width:100%" src="/resources/tristan.jpg"></div>
+            </div>
+            <div class="team-row team-row-3">
+                <div class="team-image"><img style="height:100%; width:100%" src="/resources/fabio_profile_pic.png"></div>
+                <div class="team-user user-3">
+                    <div class="team-name">40€/Month - For experts</div>
+                    <span class="team-end">.</span>
+                    <div class="team-description">
+                        <ul>
+                            <li>100 gb storage</li>
+                            <li>Unlimited users</li>
+                            <li>Free technical support</li>
+                            <li>100 mb/s bandwidth</li>
+                            <li>Unlimited devices</li>
+                            <li>Host static websites</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="team-row team-row-4">   
+                <div class="team-user user-4">
+                    <div class="team-name">Customise it</div>
+                    <span class="team-end">.</span>
+                    <div class="team-description">
+                        <ul>
+                            <li>Up to 1Tb storage</li>
+                            <li>Unlimited users</li>
+                            <li>Free technical support</li>
+                            <li>Maxmimum bandwidth</li>
+                            <li>Unlimited devices</li>
+                            <li>Host static websites</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="team-image"><img style="height:100%; width:100%" src="/resources/fabio_profile_pic.png"></div>
+            </div>
+    </section>
+
+    <div class="clear"></div>
+
+    <!--start - footer-->
+    <footer class="footer-wrapper">
+        <div class="footer-leftbox">
+            <span class="text-uppercase bold-font">LaciCloud</span><br>
+            <span class="text-italic">Made with love</span><br>
+            <span class="text-italic">Brussels, Belgium  - <a href="mailto:laci@lacicloud.net">laci@lacicloud.net</a></span>
+        </div>
+        <div class="footer-social">
+            <a href="#" class="icon tw"><img src="/resources/social-twitter.png"></a>
+            <a href="#" class="icon fb"><img src="/resources/social-facebook.png"></a>
+            <a href="#" class="icon yt"><img src="/resources/social-youtube.png"></a>
+        </div>
+    </footer>
+    <!--scripts-->
+    <script src="/js/main.js"></script>
+
+
+</body>
+</html>
