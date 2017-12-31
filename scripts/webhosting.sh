e169b5bb883736335906e02432e496015118c5a9
Molnarfi Laszlo <laci@lacicloud.net>
2017-12-31 12:06:29 +0100

    See previous commits

--- scripts/webhosting.sh
+++ scripts/webhosting.sh
@@ -107,6 +107,7 @@ echo "server {
         	try_files \$uri =404;
         	fastcgi_split_path_info ^(.+\.php)(/.+)$;
        	        fastcgi_pass unix:/var/run/php7.0-fpm-$sitename.sock;
+		fastcgi_read_timeout 30;
         	fastcgi_index index.php;
         	fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
         	include fastcgi_params;
