56ac52547ece32d61df9526945c5524950c0636e
lacicloud <laci@lacicloud.net>
2018-12-24 22:41:05 +0100

    Final spelling mistake fix on index.php for LaciCloud V2

--- www/developers/localweb/index.php
+++ www/developers/localweb/index.php
@@ -129,7 +129,7 @@ $lacicloud_api -> isPageCached();
                 <div class="section-icon"><img src="resources/open_source-icon.png" alt="The open-source logo" /></div>
                 <div class="section-heading"><h1>Open-Source</h1></div>
                 <div class="section-text pad-lr versatility-text">
-                    Open-sourcing LaciCloud allows anyone access to view and enhance the source code. As a result, bugs gets fixed faster, 
+                    Open-sourcing LaciCloud allows anyone access to view and enhance the source code. As a result, bugs get fixed faster, 
                     and anyone can verify the codebase to make sure there are no back-doors and that nothing strange is going on.
                     <br>
                     LaciCloud's Github page can be found <strong><a id="github_link" href="https://github.com/lacicloud/LaciCloud">here.</a></strong>
